4019ef7984534bc099217dec307a623e3c97013c
parkyounghwan <dudrnxps1@gmail.com>
2019-06-23 17:01:18 +0900

    update routes/todos 'list-form 연결'

--- routes/todos.js
+++ routes/todos.js
@@ -4,27 +4,27 @@ const TodosModel = require('../models/Todo');
 const moment = require('moment');
 
 router.get('/', (req, res, next) => {
-  TodosModel.find({}, (err, todos) => {
-    console.log(todos[15].created_at);
-    console.log(todos[15].updated_at);
-    if (err) return res.status(500).send('Todos 조회 실패');
-    res.render('todos_list.html', {todos, moment});
-  });
+    TodosModel.find({}, (err, todos) => {
+        console.log(todos[15].created_at);
+        console.log(todos[15].updated_at);
+        if (err) return res.status(500).send('Todos 조회 실패');
+        res.render('todos_list.html', {todos, moment});
+    });
 });
 
 router.get('/add', (req, res, next) => {
-  res.render('todos_add.html');
+    res.render('form.html', {title: 'username'});   // yhpark 수정함 (todos_add -> form 연결)
 });
 
 router.post('/add', (req, res, next) => {
-  let todo = new TodosModel({
-    title: req.body.title,
-    contents: req.body.contents,
-    date: req.body.date,
-  });
-  todo.save((err) => {
-    res.redirect('/todos/');
-  });
+    let todo = new TodosModel({
+        title: req.body.title,
+        contents: req.body.contents,
+        date: req.body.date,
+    });
+    todo.save((err) => {
+        res.redirect('/todos/');
+    });
 });
 
 module.exports = router;
