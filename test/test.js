b08ab0a233b149fa44b123dde005d0e8c9645fc1
jaeyeon kim <foodsksms@gmail.com>
2019-06-24 17:25:58 +0900

    apply mocha

--- test/test.js
+++ test/test.js
@@ -6,5 +6,22 @@ describe('#Hello World!', () => {
     // 입력값이라고 가정
     assert.equal('Hello World!', input);
   });
+  describe('#String test', () => {
+    it('Hello의 문자 개수는 5', () => {
+      const str = 'Hello';
+      if (str.length === 5) {
+        assert.ok(true);
+      } else {
+        assert.ok(false);
+      }
+    });
+    it('World는 W 대문자', () => {
+      const str = 'World';
+      if (str.indexOf('w') > -1) {
+        assert.ok(true);
+      } else {
+        assert.ok(false);
+      };
+    });
+  });
 });
-
